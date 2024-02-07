24267e5e823ca2697950bca35516425d5df4773f
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2024-02-07 14:24:41 +0100

    Merge HC port of Subtitles by ekibun (#2)

--- Subtitles.Core/Core.Subtitles.cs
+++ Subtitles.Core/Core.Subtitles.cs
@@ -1,133 +1,182 @@
 ﻿// System
+
 using System;
 
 // BepInEx
 using BepInEx;
 using BepInEx.Configuration;
-using BepInEx.IL2CPP;
 using HarmonyLib;
+#if RG
+using BepInEx.IL2CPP;
 using UnhollowerRuntimeLib;
+#elif HC
+using BepInEx.Unity.IL2CPP;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using TMPro;
+#endif
 
 // Unity
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
 
 // This file is not game specific. Its responsible to manage the Subtitle Canvas and text.
-namespace IllusionPlugins
+namespace IllusionPlugins;
+
+[BepInProcess(Constants.MainGameProcessName)]
+[BepInPlugin(GUID, PluginName, Version)]
+public partial class Subtitles : BasePlugin
 {
-    [BepInProcess(Constants.MainGameProcessName)]
-    [BepInPlugin(GUID, PluginName, Version)]
-    public partial class Subtitles : BasePlugin
+    // Plugin consts
+    public const string GUID = "SpockBauru.Subtitles";
+    public const string PluginName = "Subtitles";
+    public const string Version = "0.1";
+    public const string PluginNameInternal = Constants.Prefix + "_Subtitles";
+
+    // BepInEx Config
+    private static ConfigEntry<bool> EnableConfig;
+
+    // Plugin variables
+    private static GameObject canvasObject;
+    private static new BepInEx.Logging.ManualLogSource Log;
+
+    public override void Load()
     {
-        // Plugin consts
-        public const string GUID = "SpockBauru.Subtitles";
-        public const string PluginName = "Subtitles";
-        public const string Version = "0.1";
-        public const string PluginNameInternal = Constants.Prefix + "_Subtitles";
+        Log = base.Log;
 
-        // BepInEx Config
-        internal static ConfigEntry<bool> EnableConfig;
+        EnableConfig = Config.Bind("General",
+                                   "Enable Subtitles",
+                                   true,
+                                   "Reload the game to Enable/Disable");
 
-        // Plugin variables
-        static GameObject canvasObject;
+        if (EnableConfig.Value)
+            Harmony.CreateAndPatchAll(typeof(Hooks), GUID);
 
-        public override void Load()
+        // IL2CPP don't automatically inherits MonoBehaviour, so needs to add a component separatelly
+        ClassInjector.RegisterTypeInIl2Cpp<SubtitlesCanvas>();
+    }
+
+    /// <summary>
+    /// Create the subtitle canvas in the desired scene
+    /// </summary>
+    /// <param name="scene"></param>
+    public static void MakeCanvas(Scene scene)
+    {
+        if (canvasObject != null) 
+            Object.Destroy(canvasObject);
+
+        // Creating Canvas object
+        canvasObject = new GameObject("SubtitleCanvas");
+        SceneManager.MoveGameObjectToScene(canvasObject, scene);
+        canvasObject.AddComponent<SubtitlesCanvas>();
+    }
+
+    public class SubtitlesCanvas : MonoBehaviour
+    {
+        // Constructor needed to use Start, Update, etc...
+        public SubtitlesCanvas(IntPtr handle) : base(handle) { }
+
+        private static GameObject subtitleObject;
+#if RG
+        private static Text subtitle;
+#elif HC
+        private static TextMeshProUGUI subtitle;
+#endif
+
+        private static float time = 0;
+        private static float clipLenght = 0;
+
+        private void Start()
         {
-            EnableConfig = Config.Bind("General",
-                                 "Enable Subtitles",
-                                 true,
-                                 "Reload the game to Enable/Disable");
+            // Setting canvas attributes
+            var canvasScaler = canvasObject.AddComponent<CanvasScaler>();
+            canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            canvasScaler.referenceResolution = new Vector2(Screen.width, Screen.height);
 
-            if (EnableConfig.Value)
+            var canvas = canvasObject.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.sortingOrder = 500;
+            canvasObject.AddComponent<CanvasGroup>().blocksRaycasts = false;
+
+            // Setting subtitle object
+            subtitleObject = new GameObject("SubtitleText");
+            subtitleObject.transform.SetParent(canvasObject.transform);
+
+#if RG
+            var fontSize = (int)(Screen.height / 25.0f);
+
+            var subtitleRect = subtitleObject.AddComponent<RectTransform>();
+            subtitleRect.pivot = new Vector2(0, -1);
+            subtitleRect.sizeDelta = new Vector2(Screen.width * 0.990f, fontSize + fontSize * 0.05f);
+
+            subtitle = subtitleObject.AddComponent<Text>();
+            subtitle.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+            subtitle.fontSize = fontSize;
+            subtitle.fontStyle = FontStyle.Bold;
+            subtitle.alignment = TextAnchor.LowerCenter;
+            subtitle.horizontalOverflow = HorizontalWrapMode.Wrap;
+            subtitle.verticalOverflow = VerticalWrapMode.Overflow;
+            subtitle.color = Color.white;
+            subtitle.text = "";
+
+            var outline = subtitleObject.AddComponent<Outline>();
+            outline.effectDistance = new Vector2(2.0f, -2.0f);
+#elif HC
+            var fontSize = (int)(Screen.height / 30.0f);
+
+            var subtitleRect = subtitleObject.AddComponent<RectTransform>();
+            subtitleRect.pivot = new Vector2(0, -1);
+            subtitleRect.sizeDelta = new Vector2(Screen.width * 0.990f, fontSize + fontSize * 0.05f);
+
+            static T getResource<T>(string name) where T : Object
             {
-                Harmony.CreateAndPatchAll(typeof(Hooks), GUID);
+                var objs = Resources.FindObjectsOfTypeAll(Il2CppType.Of<T>());
+                for (var i = objs.Length - 1; i >= 0; --i)
+                {
+                    var obj = objs[i];
+                    if (obj.name == name)
+                    {
+                        var ret = obj.TryCast<T>();
+                        return ret;
+                    }
+                }
+
+                return null;
             }
 
-            // IL2CPP don't automatically inherits MonoBehaviour, so needs to add a component separatelly
-            ClassInjector.RegisterTypeInIl2Cpp<SubtitlesCanvas>();
+            subtitle = subtitleObject.AddComponent<TextMeshProUGUI>();
+            subtitle.font = getResource<TMP_FontAsset>("tmp_hc_default");
+            subtitle.fontSharedMaterial = getResource<Material>("tmp_hc_default t_outline_bold");
+
+            subtitle.fontSize = fontSize;
+            subtitle.alignment = TextAlignmentOptions.Bottom;
+            subtitle.overflowMode = TextOverflowModes.Overflow;
+            subtitle.enableWordWrapping = true;
+            subtitle.color = Color.white;
+            subtitle.text = "";
+#endif
         }
 
         /// <summary>
-        /// Create the subtitle canvas in the desired scene
+        /// Display the subtitle text while the voiceFile is active
         /// </summary>
-        /// <param name="scene"></param>
-        public static void MakeCanvas(Scene scene)
+        /// <param name="voiceFile"></param>
+        /// <param name="text"></param>
+        public static void DisplaySubtitle(AudioSource voiceFile, string text)
         {
-            if (canvasObject != null) return;
-
-            // Creating Canvas object
-            canvasObject = new GameObject("SubtitleCanvas");
-            SceneManager.MoveGameObjectToScene(canvasObject, scene);
-            canvasObject.AddComponent<SubtitlesCanvas>();
+            subtitle.text = text;
+            clipLenght = voiceFile.clip.length;
+            time = 0;
         }
 
-        public class SubtitlesCanvas : MonoBehaviour
+        // Using Update because coroutines, onDestroy and onDisable are not working as intended
+        private void Update()
         {
-            // Constructor needed to use Start, Update, etc...
-            public SubtitlesCanvas(IntPtr handle) : base(handle) { }
-
-            static GameObject subtitleObject;
-            static Text subtitle;
-
-            static float time = 0;
-            static float clipLenght = 0;
-
-            void Start()
-            {
-                // Setting canvas attributes
-                var canvasScaler = canvasObject.AddComponent<CanvasScaler>();
-                canvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-                canvasScaler.referenceResolution = new Vector2(Screen.width, Screen.height);
-
-                Canvas canvas = canvasObject.GetComponent<Canvas>();
-                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
-                canvas.sortingOrder = 500;
-                canvasObject.AddComponent<CanvasGroup>().blocksRaycasts = false;
-
-                // Setting subtitle object
-                subtitleObject = new GameObject("SubtitleText");
-                subtitleObject.transform.SetParent(canvasObject.transform);
-
-                int fontSize = (int)(Screen.height / 25.0f);
-
-                RectTransform subtitleRect = subtitleObject.AddComponent<RectTransform>();
-                subtitleRect.pivot = new Vector2(0, -1);
-                subtitleRect.sizeDelta = new Vector2(Screen.width * 0.990f, fontSize + (fontSize * 0.05f));
-
-                subtitle = subtitleObject.AddComponent<Text>();
-                subtitle.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
-                subtitle.fontSize = fontSize;
-                subtitle.fontStyle = FontStyle.Bold;
-                subtitle.alignment = TextAnchor.LowerCenter;
-                subtitle.horizontalOverflow = HorizontalWrapMode.Wrap;
-                subtitle.verticalOverflow = VerticalWrapMode.Overflow;
-                subtitle.color = Color.white;
-                subtitle.text = "";
-
-                var outline = subtitleObject.AddComponent<Outline>();
-                outline.effectDistance = new Vector2(2.0f, -2.0f);
-            }
-
-            /// <summary>
-            /// Display the subtitle text while the voiceFile is active
-            /// </summary>
-            /// <param name="voiceFile"></param>
-            /// <param name="text"></param>
-            public static void DisplaySubtitle(AudioSource voiceFile, string text)
-            {
-                subtitle.text = text;
-                clipLenght = voiceFile.clip.length;
-                time = 0;
-            }
-
-            // Using Update because coroutines, onDestroy and onDisable are not working as intended
-            void Update()
-            {
-                if (time < clipLenght) subtitleObject.active = true;
-                else subtitleObject.active = false;
-                time += Time.deltaTime;
-            }
+            if (time < clipLenght) subtitleObject.active = true;
+            else subtitleObject.active = false;
+            time += Time.deltaTime;
         }
     }
 }
---
+++ Subtitles.RG/RG_Subtitles.cs
@@ -0,0 +1,120 @@
+﻿using HarmonyLib;
+using UnhollowerBaseLib;
+using Il2CppSystem.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using static HVoiceCtrl;
+
+namespace IllusionPlugins;
+
+// This file is game specific. It must provide the scene, voice file and text to Core.Subtitles
+public partial class Subtitles
+{
+    private static Il2CppReferenceArray<Dictionary<int, Dictionary<int, Dictionary<int, Dictionary<int, VoiceListInfo>>>>> weirdDictionary;
+
+    //private static List<PlayVoiceinfo> weirdIndexes = new List<PlayVoiceinfo>();
+    private static HScene hentaiScene;
+    private static string currentFile;
+    private static string currentText;
+
+    private static class Hooks
+    {
+        // H-Scene initialization
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HScene), nameof(HScene.Start))]
+        private static void HSceneStart(HScene __instance)
+        {
+            weirdDictionary = __instance.CtrlVoice._voiceList.DicDicDicDicVoice;
+            hentaiScene = __instance;
+            // Making the Subtitle Canvas
+            MakeCanvas(SceneManager.GetActiveScene());
+        }
+
+        // Play all voices
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Manager.Voice), nameof(Manager.Voice.PlayStandby))]
+        private static void GetTextFromVoice(Manager.Voice.Loader loader, AudioSource audioSource)
+        {
+            if (hentaiScene == null) return;
+
+            var audioFile = loader.Asset;
+
+            //Debug.Log(" audioFile: " + audioFile);
+
+            //// THIS WAY WOULD BE MORE OPTIMIZED, BUT IL2CPP CANNOT GET THE INDEXES...
+            //for (int femaleIndex = 0; femaleIndex < weirdDictionary.Count; femaleIndex++)
+            //{
+            //    var female = weirdDictionary[femaleIndex];
+            //    if (female == null) continue;
+
+            //    // Each file is picked at random. Usually there are not more than 8 files
+            //    for (int i = 0; i < weirdIndexes.Count; i++)
+            //    {
+            //        currentFile = female[weirdIndexes[i].Mode][weirdIndexes[i].Sheet][weirdIndexes[i].Kind][weirdIndexes[i].VoiceID].NameFile;
+            //        Debug.Log("NameFile: " + currentFile);
+            //        if (currentFile == audioFile)
+            //        {
+            //            currentText = female[weirdIndexes[i].Mode][weirdIndexes[i].Sheet][weirdIndexes[i].Kind][weirdIndexes[i].VoiceID].Word;
+            //            //Debug.Log("File: " + audioSource.gameObject.name + " Text: " + currentText);
+            //            Debug.Log(currentText);
+            //            SubtitlesCanvas.DisplaySubtitle(audioSource.gameObject, currentText);
+            //        }
+            //    }
+            //}
+
+            // Searching all dictionary... Its dumb but IL2CPP is not cooperating
+            foreach (var female in weirdDictionary)
+            {
+                if (female == null) continue;
+                foreach (var mode in female.Values)
+                    foreach (var sheet in mode.Values)
+                        foreach (var kind in sheet.Values)
+                            foreach (var voiceID in kind.Values)
+                            {
+                                currentFile = voiceID.NameFile;
+                                if (currentFile == audioFile)
+                                {
+                                    currentText = voiceID.Word;
+                                    // Send the text to be displayed
+                                    SubtitlesCanvas.DisplaySubtitle(audioSource, currentText);
+                                    break;
+                                }
+                            }
+            }
+        }
+
+
+        //// Voice in the beginning - ILCCPP CANT HANDLE THIS METHOD! MANY SUBTITLES MISSING BECAUSE OF IT!!!!!!
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(HVoiceCtrl), nameof(HVoiceCtrl.GetPlayListNum))]
+        //private static void GetDicIncexesBegining(ref List<PlayVoiceinfo> __result)
+        //{
+        //    weirdIndexes = __result;
+        //}
+
+        //// General Voices
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(HVoiceCtrl), nameof(HVoiceCtrl.GetPlayNum))]
+        //private static void GetDicIncexes(ref List<PlayVoiceinfo> __result)
+        //{
+        //    weirdIndexes = __result;
+        //}
+
+        //// Voice when the position change
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(HVoiceCtrl), nameof(HVoiceCtrl.GetStartPlayNum))]
+        //private static void GetDicIncexesStart(ref List<PlayVoiceinfo> __result)
+        //{
+        //    weirdIndexes = __result;
+        //}
+
+        //// Voice in the first position
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(HVoiceCtrl), nameof(HVoiceCtrl.GetStartPlayListNum))]
+        //private static void GetDicIncexesBegining(ref List<PlayVoiceinfo> __result)
+        //{
+        //    weirdIndexes = __result;
+        //}
+    }
+}
